f5f1772256fb85e14346dbf070f539d60591cdf2
developwithpranjal <pranjalbros123@gmail.com>
2026-02-03 16:04:49 +0530

    incomplete

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let box = document.querySelector(".form-box");
 let leftbar = document.querySelector(".leftbar");
 let rightbar = document.querySelector(".rightbar");
-
+let newbtn = document.querySelector(".newbtn");
 let components = [
   {
     label: "Name",
@@ -45,7 +45,11 @@ components.forEach((item) => {
 function showinput(item) {
   let data = document.createElement("div");
   data.classList.add("field");
+  let delbtn = document.createElement("button");
+  delbtn.classList.add("delbtn");
+  delbtn.innerHTML = "Delete";
 
+  data.append(delbtn);
   if (item.type !== "button") {
     let label = document.createElement("label");
     label.innerText = item.label;
@@ -54,28 +58,60 @@ function showinput(item) {
   if (item.type === "radio") {
     let gender = document.createElement("div");
     gender.classList.add("gender");
-    gender.innerHTML = `<label><input type="radio" name="Gender"> Male</label>
+    gender.innerHTML = `<label> <input type="radio" name="Gender" > Male</label>
             <label> <input type="radio" name="Gender"> Female</label>
             <label> <input type="radio" name="Gender"> Others</label>`;
-            data.append(gender);
-            box.append(data)
-            return;
+    data.append(gender);
+    box.append(data);
+    return;
   }
+  delbtn.addEventListener("click", () => {
+    data.remove();
+  });
+
   let input = document.createElement("input");
   input.type = item.type;
+
+  input.readOnly = true;
 
   if (item.type === "button") {
     input.value = "Submit";
   } else {
-    input.placeholder = "Enter " + item.label;
+    input.placeholder = "Enter placeholder's text...";
   }
 
   console.log(item);
 
-  rightbar.innerHTML = `<h2>PROPERTIES</h2>
-        <p>Select an element to edit its properties</p>
-    <label class= "label">Label:<input placeholder = "Enter Label"></label>
-    <label class= "placeholder" >Placeholder:<input placeholder = "Enter placeholder"></label>`;
   data.append(input);
   box.append(data);
+  data.addEventListener("click", () => editData(data));
 }
+
+function editData(data) {
+  rightbar.innerHTML = `<h2>PROPERTIES</h2>
+        <p>Select an element to edit its properties</p>
+    <label class= "label">Label:<input placeholder = "Enter label" ></label>
+    <label class= "placeholder" >Placeholder:<input placeholder = "Enter placeholder" ></label>`;
+
+  let rightlabel = document.querySelector(".label input");
+  let placeholder = document.querySelector(".placeholder input");
+  let fieldLabel = data.querySelector("label");
+  let fieldInput = data.querySelector("input");
+
+  rightlabel.oninput = () => {
+    if (fieldLabel) {
+      fieldLabel.innerText = rightlabel.value;
+    }
+  };
+
+  placeholder.oninput = () => {
+    if (fieldInput) {
+      fieldInput.placeholder = placeholder.value;
+    } else {
+    }
+  };
+}
+newbtn.addEventListener("click", () => {
+  box.innerHTML = "";
+  rightbar.innerHTML = "";
+});
